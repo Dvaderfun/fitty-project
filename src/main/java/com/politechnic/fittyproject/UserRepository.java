56e3cee35b8ce76e4758213566f8e4f620eee44c
Darth Vader <dvaderfun@gmail.com>
2019-09-18 17:57:58 +0300

    Тесты и Rest (?)

---
+++ src/main/java/com/politechnic/fittyproject/UserRepository.java
@@ -0,0 +1,9 @@
+package com.politechnic.fittyproject;
+
+
+import com.politechnic.fittyproject.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {}
