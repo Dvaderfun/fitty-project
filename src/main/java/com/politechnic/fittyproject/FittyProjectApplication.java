92f3b2a6ee187c56340d773f8e198cc4c9584476
Darth Vader <dvaderfun@gmail.com>
2019-09-18 17:06:12 +0300

    Initial commit

---
+++ src/main/java/com/politechnic/fittyproject/FittyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.politechnic.fittyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FittyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FittyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/politechnic/fittyproject/FittyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.politechnic.fittyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FittyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
